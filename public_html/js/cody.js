1d9f170ddedfc22aea458e7b511df6da525f09ac
yvo <yvo@eveg.vegian>
2016-10-13 18:55:10 +0200

    Module JQuery

--- public_html/js/cody.js
+++ public_html/js/cody.js
@@ -4,13 +4,49 @@ function blocaa(nomFichCible) {
 }
 
 function blocbb() {
+    //$('#1').attr("id");
     //$('.contenu').load('blocb.html');
     var a = "";//$('.menu').val();
-    a = $('.menu input').val();
+    //a = $('.menu input').val();
+    a = $('.menu input').id();
+    //$('.menu input').attr("id");
     // $('.contenu').html('$(('.menu').input).val()');
-    if (a=="bouton1") {$('.contenu').load("bloca.html"); a="";}
-    if (a=="bouton2") {$('.contenu').load("blocb.html"); a="";}
-    if (a=="bouton3") {$('.contenu').load("blocc.html"); a="";}
-   // $('.contenu').html(a);
+    if (a == "#1") {
+        $('.contenu').load("bloca.html");
+        a = "";
+    }
+    if (a == "#2") {
+        $('.contenu').load("blocb.html");
+        a = "";
+    }
+    if (a == "#3") {
+        $('.contenu').load("blocc.html");
+        a = "";
+    }
+    // $('.contenu').html(a);
     //alert(a);
+    // $( "input" ).button( $( "p" ).text($(this).val()));
 }
+
+function bloccc(a) {
+    if (a == "1") {
+        $('.contenu').load("bloca.html");
+    }
+    if (a == "2") {
+        $('.contenu').load("blocb.html");
+    }
+    if (a == "3") {
+        $('.contenu').load("blocc.html");
+    }
+}
+
+       function chargejson() {
+                    $.getJSON("json/films.json", function (data){
+                        var tabfilms = data.films;
+                        var a="";
+                        for (i=0;i<tabfilms.length;i++) {
+                            a +=" <p>nom du film:" +tabfilms[i].titre + "<br> synopsis: "+tabfilms[i].synopsis  +"</p>" ;
+                            $('#contenu').html(a);
+                        }
+                    });
+    }
